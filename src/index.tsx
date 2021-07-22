b7f21a39060989a36c6fef1f6fa31c567adf2d29
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-22 17:47:14 +0200

    added browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import * as React from 'react';
+import { ColorModeScript } from '@chakra-ui/react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root"),
-)
+  document.getElementById('root')
+);
