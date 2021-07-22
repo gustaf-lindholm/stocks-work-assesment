3402f38ec426862c5aa626cbac0cfc614f395278
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-22 15:06:37 +0200

    styling on search result

--- src/Components/SearchResults.tsx
+++ src/Components/SearchResults.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { List, ListItem, ListIcon, Text } from '@chakra-ui/react';
+import { List, ListItem, ListIcon, Text, Spacer, Button } from '@chakra-ui/react';
 
 interface Stock {
   "1. symbol": string;
@@ -18,14 +18,15 @@ const SearchResults: React.FC<{ searchResult: Stock[] }> = ({ searchResult }) =>
   
   if (!searchResult) return <Text>Search for stocks to see them here</Text>;
   return (
-    <List spacing={3}>
+    <List spacing={3} p="4">
       {searchResult.map((item, index) => {
         return (
-          <ListItem key={searchResult[index]['1. symbol']}>
-            <Text>
+          <ListItem shadow="md" border="0.25px solid gray" p="2" display="flex" flexDirection="row" alignItems="center" key={searchResult[index]['1. symbol']}>
+            <Text isTruncated maxW="80%">
               {`${searchResult[index]['2. name']} - ${searchResult[index]['2. name']}`}
             </Text>
-            &#9744;
+            <Spacer/>
+            <Button>&#9734;</Button>
           </ListItem>
         );
       })}
