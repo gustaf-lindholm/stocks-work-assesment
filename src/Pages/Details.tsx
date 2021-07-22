39e81c4fe15dbf27f43a9d3bf70de70ff4628eca
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-22 18:22:17 +0200

    fetch details and display them

--- src/Pages/Details.tsx
+++ src/Pages/Details.tsx
@@ -1,6 +1,6 @@
+import * as React from 'react';
 import { Heading } from '@chakra-ui/layout';
 import { Box, Text } from '@chakra-ui/react';
-import * as React from 'react';
 import { useParams } from 'react-router';
 import { IStockDetails } from '../Interfaces/StockInterface';
 
@@ -12,15 +12,24 @@ const Details = () => {
   const { stockSymbol }: Params = useParams();
   const [stockDetails, setStockDetails] = React.useState<IStockDetails | null>(null);
 
+  function currencyFormatter(currency: string, MarketCapitalization: number) {
+    return new Intl.NumberFormat('sv-SE', {
+      style: 'currency',
+      currency: currency,
+      maximumSignificantDigits: 3
+    }).format(MarketCapitalization);
+  }
+
   React.useEffect(() => {
     const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stockSymbol}&apikey=HTJRJGUCDN10NMV9`;
 
-    const stockDetails = async () => {
+    const getStockDetails = async () => {
       try {
         const response = await fetch(url);
 
         if (response.ok) {
           const details = await response.json();
+
           setStockDetails(details);
         }
       } catch (error) {
@@ -28,15 +37,15 @@ const Details = () => {
       }
     };
 
-    stockDetails();
+    getStockDetails();
   }, []);
 
-  if (!stockDetails) return <Heading>Something went wrong...</Heading>
+  if (!stockDetails) return <Heading>Something went wrong...</Heading>;
   return (
     <Box>
       <Heading>{stockDetails.Name}</Heading>
       <Text>{stockDetails.Address}</Text>
-      <Text>{stockDetails.MarketCapitalization}</Text>
+      <Text>{currencyFormatter(stockDetails.Currency, Number(stockDetails.MarketCapitalization))}</Text>
       <Text>{stockDetails.Description}</Text>
     </Box>
   );
