2bced26dec80280bb4f940cfdd6c1a8301903810
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-22 16:02:43 +0200

    adding to and showing portfolio

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,7 +1,9 @@
 import { Box, Table, TableCaption, Tbody, Td, Th, Thead, Tr, Button } from '@chakra-ui/react';
 import * as React from 'react';
+import { IStock } from '../Interfaces/StockInterface';
+import { nanoid } from 'nanoid';
 
-const Portfolio = () => {
+const Portfolio: React.FC<{ portfolio: IStock[] | [] }> = ({ portfolio }) => {
   return (
     <Box>
       <Table variant="striped" colorScheme="gray">
@@ -14,11 +16,17 @@ const Portfolio = () => {
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>Apple</Td>
-            <Td>AAPL</Td>
-            <Td><Button>Remove</Button></Td>
-          </Tr>
+          {portfolio.map((item) => {
+            return (
+              <Tr key={nanoid()}>
+                <Td>{item['2. name']}</Td>
+                <Td>{item['1. symbol']}</Td>
+                <Td>
+                  <Button>Remove</Button>
+                </Td>
+              </Tr>
+            );
+          })}
         </Tbody>
       </Table>
     </Box>
--- src/Components/SearchResults.tsx
+++ src/Components/SearchResults.tsx
@@ -1,32 +1,32 @@
 import * as React from 'react';
 import { List, ListItem, ListIcon, Text, Spacer, Button } from '@chakra-ui/react';
+import { IStock } from '../Interfaces/StockInterface';
 
-interface Stock {
-  "1. symbol": string;
-  "2. name": string;
-  "3.type": string;
-  "4. region": string;
-  "5. marketOpen": string;
-  "6. marketClose": string;
-  "7. timezone": string;
-  "8. currency": string;
-  "9. matchScore": string;
-}
-
-const SearchResults: React.FC<{ searchResult: Stock[] }> = ({ searchResult }) => {
+const SearchResults: React.FC<{
+  searchResult: IStock[];
+  onAddToPortfolio: (stock: IStock) => void;
+}> = ({ searchResult, onAddToPortfolio }) => {
   React.useEffect(() => {}, []);
-  
+
   if (!searchResult) return <Text>Search for stocks to see them here</Text>;
   return (
     <List spacing={3} p="4">
       {searchResult.map((item, index) => {
         return (
-          <ListItem shadow="md" border="0.25px solid gray" p="2" display="flex" flexDirection="row" alignItems="center" key={searchResult[index]['1. symbol']}>
+          <ListItem
+            shadow="md"
+            border="0.25px solid gray"
+            p="2"
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            key={searchResult[index]['1. symbol']}
+          >
             <Text isTruncated maxW="80%">
               {`${searchResult[index]['2. name']} - ${searchResult[index]['2. name']}`}
             </Text>
-            <Spacer/>
-            <Button>&#9734;</Button>
+            <Spacer />
+            <Button onClick={() => onAddToPortfolio(searchResult[index])}>&#9734;</Button>
           </ListItem>
         );
       })}
