ba94e32df561c684130025000bc8d31aee9f4d11
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-27 17:18:47 +0200

    passing props and loading state for search

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -3,21 +3,29 @@ import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import { IStock } from '../Interfaces/StockInterfaces';
 
-const Search: React.FC<{ onAddToPortfolio: (stock: IStock) => void }> = ({ onAddToPortfolio }) => {
+const Search: React.FC<{
+  setPortfolioHandler: (stock: IStock) => void;
+  portfolio: IStock[];
+  isLoading: boolean;
+  startLoading: () => void;
+  stopLoading: () => void;
+}> = ({ setPortfolioHandler, portfolio, isLoading, startLoading, stopLoading }) => {
+
   const [searchResult, setSearchResult] = React.useState<[] | undefined>();
-  const [isLoading, setIsLoading] = React.useState(false);
+  // loading state for searching
+  const [searchIsLoading, setSearchIsLoading] = React.useState(false)
 
   return (
     <>
-      <SearchBar
-        setSearchResult={setSearchResult}
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-      />
+      <SearchBar setSearchIsLoading={setSearchIsLoading} setSearchResult={setSearchResult} />
       <SearchResults
         searchResult={searchResult!}
-        onAddToPortfolio={onAddToPortfolio}
+        portfolio={portfolio}
+        setPortfolioHandler={setPortfolioHandler}
         isLoading={isLoading}
+        startLoading={startLoading}
+        stopLoading={stopLoading}
+        searchIsLoading={searchIsLoading}
       />
     </>
   );
