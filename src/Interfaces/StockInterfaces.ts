f1cf5a191132ef2a85d4a83be88cc95c78281808
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-29 14:39:56 +0200

    added error message to search result interface

--- src/Interfaces/StockInterfaces.ts
+++ src/Interfaces/StockInterfaces.ts
@@ -1,17 +1,18 @@
 export interface ISymbolSearchResult {
-  bestMatches: IStock[]
+  bestMatches?: IStock[];
+  ['Error Message']?: string;
 }
 
 export interface IStock {
-  "1. symbol": string;
-  "2. name": string;
-  "3.type": string;
-  "4. region": string;
-  "5. marketOpen": string;
-  "6. marketClose": string;
-  "7. timezone": string;
-  "8. currency": string;
-  "9. matchScore": string;
+  '1. symbol': string;
+  '2. name': string;
+  '3.type': string;
+  '4. region': string;
+  '5. marketOpen': string;
+  '6. marketClose': string;
+  '7. timezone': string;
+  '8. currency': string;
+  '9. matchScore': string;
 }
 
 export interface IStockDetails {
