2055345046edca1997d0c6f1a97ac2ab948464b4
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-29 11:50:17 +0200

    search using useHttp hook

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -3,42 +3,60 @@ import * as React from 'react';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import { IStock, ISymbolSearchResult } from '../Interfaces/StockInterfaces';
-import useFetch from '../hooks/use-fetch';
+import useHttp from "../hooks/use-http";
+import { Heading, Text } from "@chakra-ui/react";
 
 const Search: React.FC<{ onAddToPortfolio: (stock: IStock) => void }> = ({ onAddToPortfolio }) => {
   const [searchParam, setSearchParam] = React.useState('');
-  const [searchResult, setSearchResult] = React.useState<IStock[] | undefined>();
-
-  // får tillbaks en funktion som tar två parametrar (requestParams, dataHandler(data))
-  // dataHandler tar data av generic type
-  const { isLoading, hasError, sendRequest } = useFetch();
+  const [searchResult, setSearchResult] = React.useState<IStock[]>([]);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   const apiKey = undefined;
 
+  // const url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey=demo";
   const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchParam}&apikey=${apiKey}`;
 
-  React.useEffect(() => {
-    function dataHandler <T extends ISymbolSearchResult>(searchResult: T): void {
-      setSearchResult(searchResult.bestMatches);
-      console.log('SEARCH: ', searchParam);
-    }
-    /**
-     * Argument of type '<T extends ISymbolSearchResult>(searchResult: T) => void' is not assignable to parameter of type 'DataHandler'.
-  Types of parameters 'searchResult' and 'data' are incompatible.
-    Type 'T' is not assignable to type 'ISymbolSearchResult'.ts(2345)
-     */
+  const [isLoading, hasError, sendRequest] = useHttp<ISymbolSearchResult>();
 
-    searchParam.length > 2 && sendRequest({ url: url }, dataHandler);
-  }, [searchParam]);
+
+  function dataHandler(data: ISymbolSearchResult): void {
+    const recievedData: ISymbolSearchResult = data; 
+    
+    if(data["Error Message"]) {
+      setErrorMessage(data["Error Message"])
+    }
+
+    if(data.bestMatches) {
+      setSearchResult(data.bestMatches)
+    }
+    console.log("Recieved:", recievedData);
+  }
+
+  React.useEffect(() => {
+    const requestParams = {
+      url: url
+    }
+
+    searchParam.length > 3 && sendRequest(requestParams, dataHandler);
+  }, [searchParam, sendRequest, url])
+
 
   return (
     <>
       <SearchBar setSearchParam={setSearchParam} isLoading={isLoading} />
-      <SearchResults
-        searchResult={searchResult!}
+      {hasError || errorMessage 
+      ? 
+        <>
+          <Heading>Error</Heading>
+          <Text>{errorMessage}</Text>
+        </>
+      :
+        <SearchResults
+        searchResult={searchResult}
         onAddToPortfolio={onAddToPortfolio}
         isLoading={isLoading}
-      />
+        />
+    }
     </>
   );
 };
