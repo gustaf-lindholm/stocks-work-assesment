04b66e8274bbfb67723066482e87efbbab2db80a
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-31 13:39:09 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
-import * as React from 'react';
-import { ChakraProvider, theme, Container } from '@chakra-ui/react';
-import { Route } from 'react-router-dom';
-import Header from './Components/Header';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+import * as React from "react";
+import { ChakraProvider, theme, Container } from "@chakra-ui/react";
+import { Route } from "react-router-dom";
+import Header from "./Components/Header";
+import StartPage from "./Pages/Startpage";
+import Details from "./Pages/Details";
 
-import StartPage from './Pages/Startpage';
-import Details from './Pages/Details';
-import { IStock } from './Interfaces/StockInterfaces';
-
-const App = () => {
-  const [portfolio, setPortfolio] = React.useState<IStock[]>([]);
-
-  const onAddToPortfolio = (stock: IStock) => {
-    setPortfolio((currentPortfolio) => {
-      return currentPortfolio?.concat(stock);
-    });
-  };
-
-  const onRemoveFromPortfolio = (id: string) => {
-    setPortfolio((currentPortfolio) => {
-      return currentPortfolio.filter((stock, index) => {
-        return id !== currentPortfolio[index]['1. symbol'];
-      });
-    });
-  };
-
+const App: React.FC = () => {
   return (
     <ChakraProvider theme={theme}>
       <Container maxW="container.lg">
         <Header />
         <Route exact path="/">
-          <StartPage
-            portfolio={portfolio}
-            onAddToPortfolio={onAddToPortfolio}
-            onRemoveFromPortfolio={onRemoveFromPortfolio}
-          />
+          <StartPage />
         </Route>
         <Route path="/details/:stockSymbol">
           <Details />
