929c43bce45bc617c4f86513e786bc9af178df03
Gustaf Lindholm <babiluskus@gmail.com>
2021-07-31 11:50:35 +0200

    I for returndata and stock

--- src/Interfaces/StockInterfaces.ts
+++ src/Interfaces/StockInterfaces.ts
@@ -1,18 +1,24 @@
 export interface ISymbolSearchResult {
   bestMatches?: IStock[];
-  ['Error Message']?: string;
+  ["Error Message"]?: string;
 }
 
 export interface IStock {
-  '1. symbol': string;
-  '2. name': string;
-  '3.type': string;
-  '4. region': string;
-  '5. marketOpen': string;
-  '6. marketClose': string;
-  '7. timezone': string;
-  '8. currency': string;
-  '9. matchScore': string;
+  "1. symbol": string;
+  "2. name": string;
+  "3. type": string;
+  "4. region": string;
+  "5. marketOpen": string;
+  "6. marketClose": string;
+  "7. timezone": string;
+  "8. currency": string;
+  "9. matchScore": string;
+  isAdded?: boolean;
+}
+
+export interface IPortfolioStock {
+  id: string;
+  data: IStock;
 }
 
 export interface IStockDetails {
@@ -22,4 +28,17 @@ export interface IStockDetails {
   MarketCapitalization: string;
   Description: string;
   Currency: string;
+}
+
+export interface IRequestParams extends RequestInit {
+  url: string;
+  method?: string;
+  headers?: HeadersInit;
+  body?: BodyInit | null;
+}
+
+export interface IReturnData<T> {
+  data: T;
+  statusCode: number;
+  statusText: string;
 }
